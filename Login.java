271aa1167769203dbc50abfccc57dd6bbbb5573d
Adham <adhamhatem.uni@gmail.com>
2023-08-14 22:34:18 +0300

    Login Function set if we're using a map for the log in data (Email and password)
    Let me know if we're using different things for the driver and the user things like that should be easy to make

---
+++ Login.java
@@ -0,0 +1,16 @@
+public class Login {
+    boolean verify = false;
+
+
+
+    public boolean Login(String password, String Email){
+
+        if(admin.containsKey(Email) && admin.constaintsValue(Password)){return verify = true;}
+        else {return verify;}
+
+
+    }
+
+
+
+}
